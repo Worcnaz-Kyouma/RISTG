44c30c2e9dca4d98b33c53ae2ceb0a774279072b
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-05-31 19:47:46 -0300

    Projectplanning 3

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -1,1 +1,18 @@
-//Generate SQL Raw Text, Make archive, Download stuff...?
+function generateFiles(external, merge){
+    let files = {
+        external: generateExternal(external),
+        mergeStg: generateMerge(merge),
+        merge: generateMerge(merge)
+    }
+    return files
+}
+
+function generateExternal(external){
+
+}
+
+function generateMerge(merge){
+
+}
+
+module.exports = generateExternal
