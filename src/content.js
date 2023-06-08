9f4f0600639dc1c20ea5293a8959696b729e9d26
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-06-08 13:31:14 -0300

    functional logic of generating files 1 and content.js filled

--- src/content.js
+++ src/content.js
@@ -1,10 +1,23 @@
-const db ={
+const pseudoDb ={
     origins: [
+        EBS = {
+            databaseName: "Mirror2EBS",
+            polyBaseLocation: ["OracleEBS", "GOLD.APPS.XXFR_"],
+            tableConvention: (externalName) => externalName.toLowerCase(),
+            mergeConvention: (externalName, type) => `SP_FRISIA_MRG_${type}_${externalName.toUpperCase()}`
 
+            
+        },
+        SIF = {
+            databaseName: "Mirror1SIF",
+            polyBaseLocation: ["OracleBCAP", "BCAP.ORASQL."],
+            tableConvention: (externalName) => "sif_" + externalName.toLowerCase(),
+            mergeConvention: (externalName, type) => `SP_FRISIA_MRG_SIF_${type}_${externalName.toUpperCase()}`
+        }
     ],
     general: {
         
     }
 }
 
-export default db;
+export default pseudoDb;
