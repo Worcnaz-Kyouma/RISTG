4d26014f7d0d2967ae0a806dd79206bf4153e933
Worcnaz Prado <108674581+Worcnaz-Kyouma@users.noreply.github.com>
2023-06-24 14:45:18 -0300

    file generating improve format

--- src/Utils/index.js
+++ src/Utils/index.js
@@ -8,8 +8,8 @@ function processRawColumn(rawColumn){
         
     }
     column.name = rawColumn.slice(0, rawColumn.indexOf(']'))
-    column.type = rawColumn.slice(rawColumn.indexOf('] ')+1, rawColumn.indexOf(')')+1)
-    if(rawColumn[rawColumn.lenght-1]!=')'){
+    column.type = rawColumn.slice(rawColumn.indexOf(']')+2, rawColumn.indexOf(')')+1)
+    if(rawColumn[rawColumn.length-1]!=')'){
         let collatePosition = rawColumn.search('COLLATE')
         if(collatePosition!=-1){
             column.options.collate = 'COLLATE ' + rawColumn.slice(collatePosition+8)
@@ -37,53 +37,53 @@ export function generateFiles(form){
 }
 
 function generateExternalBlob(databaseName, externalName, processedSource, origin, stgMergeName, tablesName){
-    const lenghtLongestColumnName = processedSource.map(column => column.name).reduce((longestColumn, currentColumn) => currentColumn.lenght>longestColumn.lenght ? currentColumn : longestColumn).lenght
+    const lengthLongestColumnName = processedSource.map(column => column.name).reduce((longestColumn, currentColumn) => currentColumn.length>longestColumn.length ? currentColumn : longestColumn).length
     
-    const numOfAdditionalTabs = (column) => {return  -Math.round(-(lenghtLongestColumnName-column.length)/8)}
+    const numOfAdditionalTabs = (columnLength) => {return  ((columnLength+2)%4!=0 && 1) + ((lengthLongestColumnName+6-((lengthLongestColumnName+2)%4||4))-(columnLength+6-((columnLength+2)%4||4)))/4}
+
+    console.log(numOfAdditionalTabs(processedSource[0].name.length));
 
     const columnsAccumulated = (haveCollate=false) => processedSource.slice(1).reduce((accumulator, currentColumn) => {
         return (
-        `${accumulator}
-        ,   [${currentColumn.name}]${[...Array(1+numOfAdditionalTabs(column))].map((cell=>cell="\t")).join('')}${currentColumn.type}\t${(haveCollate) ? currentColumn.options.collate || "NOT NULL" : "NOT NULL"}
-        `
+`${accumulator}\n,   [${currentColumn.name}]${[...Array(1+numOfAdditionalTabs(currentColumn.name.length))].map((()=>"\t")).join('')}${currentColumn.type}\t${(haveCollate) ? currentColumn.options.collate || "NOT NULL" : "NOT NULL"}`
         )
-      }, `  [${processedSource[0].name}]    ${processedSource[0].type}     ${processedSource[0].options.collate || "NOT NULL"}` )
+      }, `[${processedSource[0].name}]${[...Array(1+numOfAdditionalTabs(processedSource[0].name.length))].map((()=>"\t")).join('')}${processedSource[0].type}\t${processedSource[0].options.collate || "NOT NULL"}`)
     
     const externalText = 
-    `use ${databaseName}
+`use ${databaseName}
 
-    CREATE EXTERNAL TABLE [dbo].${externalName}
-    (
-      ${columnsAccumulated(true)}
-    )
-    WITH (DATA_SOURCE = [${origin[0]}],LOCATION = N'${origin[1]+externalName}')
-    GO
-    
-    --Teste external
-    --select * from ${externalName}
-    
-    use Frisia
-    --select * from PARAMETRO order by 1 desc
-    insert into parametro values ( (select max(cd_parametro) + 1 from PARAMETRO), 
-                                    '${stgMergeName}', 
-                                    'NOME DA PROCEDURE', 
-                                    -1, 
-                                    getdate(),
-                                    (select cast(getdate() -1 as date))
-                                  )
-    
-    CREATE TABLE ${tablesName.stgTableName}
-    (
-        ${columnsAccumulated(false)}            
-    )
-    
-    CREATE TABLE ${tablesName.tableName}
-    (
-        ${columnsAccumulated(false)}
-        , DT_INCLUIDO_DW				DATETIME
-        , DT_ALTERADO_DW				DATETIME
-    )
-    `
+CREATE EXTERNAL TABLE [dbo].${externalName}
+(
+    ${columnsAccumulated(true)}
+)
+WITH (DATA_SOURCE = [${origin[0]}],LOCATION = N'${origin[1]+externalName}')
+GO
+
+--Teste external
+--select * from ${externalName}
+
+use Frisia
+--select * from PARAMETRO order by 1 desc
+insert into parametro values ( (select max(cd_parametro) + 1 from PARAMETRO), 
+                                '${stgMergeName}', 
+                                'NOME DA PROCEDURE', 
+                                -1, 
+                                getdate(),
+                                (select cast(getdate() -1 as date))
+                                )
+
+CREATE TABLE ${tablesName.stgTableName}
+(
+    ${columnsAccumulated(false)}            
+)
+
+CREATE TABLE ${tablesName.tableName}
+(
+    ${columnsAccumulated(false)}
+,   [DT_INCLUIDO_DW]${[...Array(1+numOfAdditionalTabs(14))].map((()=>"\t")).join('')}DATETIME
+,   [DT_ALTERADO_DW]${[...Array(1+numOfAdditionalTabs(14))].map((()=>"\t")).join('')}DATETIME
+)
+`
     
     //return externalText;
     return new Blob([externalText], {type: "application/sql"})
@@ -92,56 +92,56 @@ function generateExternalBlob(databaseName, externalName, processedSource, origi
 function generateMergeBlob(mergeName, targetTable, sourceTable, processedSource, mergeKeys, whereClause, databaseName=false){
     const updateSetText = processedSource.slice(1).reduce((accumulator, currentColumn) => {
         return (
-        `${accumulator}
-        ,   [${currentColumn.name}] = SOURCE.[${currentColumn.name}]
-        `
+`${accumulator}
+,   [${currentColumn.name}] = SOURCE.[${currentColumn.name}]
+`
         )
       }, `   [${processedSource[0].name}] = SOURCE.[${processedSource[0].name}]`) 
     
     const insertSetText = (isSource=false) => processedSource.slice(1).reduce((accumulator, currentColumn) => {
         return (
-        `${accumulator}
-        ,   ${isSource && "SOURCE."}[${currentColumn.name}]
-        `
+`${accumulator}
+,   ${isSource && "SOURCE."}[${currentColumn.name}]
+`
         )
       }, `   ${isSource && "SOURCE."}[${processedSource[0].name}]`) 
     
     const mergeText = 
-    `USE FRISIA
-    GO
-    
-    create or ALTER procedure [dbo].${mergeName}  ${(databaseName) ? "@ControlaData bit" : ""} as 
-    
-    ${(databaseName) ? "truncate table " + targetTable : ""}
-    
-    MERGE ${targetTable} as TARGET
-        USING (
-                SELECT * FROM ${databaseName ? databaseName + ".dbo." : ""}${sourceTable}
-                    ${whereClause ? whereClause : ""}	
-              ) 
-              AS SOURCE ON (                      		 
-                    ${mergeKeys}						
-              )
-        WHEN MATCHED 
-        THEN 
-            UPDATE SET 
-                   ${updateSetText}
-                   ${!databaseName && ", DT_ALTERADO_DW = GETDATE()"}								
-                
-        WHEN NOT MATCHED THEN
-            INSERT (				 
-                    ${insertSetText()}
-                    ${!databaseName && ",    DT_INCLUIDO_DW"} 					                
+`USE FRISIA
+GO
+
+create or ALTER procedure [dbo].${mergeName}  ${(databaseName) ? "@ControlaData bit" : ""} as 
+
+${(databaseName) ? "truncate table " + targetTable : ""}
+
+MERGE ${targetTable} as TARGET
+    USING (
+            SELECT * FROM ${databaseName ? databaseName + ".dbo." : ""}${sourceTable}
+                ${whereClause ? whereClause : ""}	
+            ) 
+            AS SOURCE ON (                      		 
+                ${mergeKeys}						
             )
-            VALUES 
-            (
-                   ${insertSetText(true)}
-                   ${!databaseName && ",    GETDATE()"}   				        
-            );
-    update 	Frisia.dbo.PARAMETRO  set [DT_EXECUCAO] = getdate() 
-    where [NM_PARAMETRO] = ${mergeName}
-    GO
-    `
+    WHEN MATCHED 
+    THEN 
+        UPDATE SET 
+                ${updateSetText}
+                ${!databaseName && ", DT_ALTERADO_DW = GETDATE()"}								
+            
+    WHEN NOT MATCHED THEN
+        INSERT (				 
+                ${insertSetText()}
+                ${!databaseName && ",    DT_INCLUIDO_DW"} 					                
+        )
+        VALUES 
+        (
+                ${insertSetText(true)}
+                ${!databaseName && ",    GETDATE()"}   				        
+        );
+update 	Frisia.dbo.PARAMETRO  set [DT_EXECUCAO] = getdate() 
+where [NM_PARAMETRO] = ${mergeName}
+GO
+`
 
     //return mergeText;
     return new Blob([mergeText], {type: "application/sql"})
